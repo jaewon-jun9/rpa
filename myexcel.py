fd3f0daa503ce9017c2763ba2afce73e31cb7f08
jaewon-jun9 <jjw0525@naver.com>
2020-09-30 22:22:22 +0900

    merge function, main code update

--- myexcel.py
+++ myexcel.py
@@ -30,6 +30,14 @@ def numToExNum(i,j):
     col.append(j)
     excol=''.join(col)
     return (excol)
-def appExcelList(num1,ws):
-    for row in readCell(num1):
+def appExcelList(num1,ws,load_ws):
+    for row in readCell(num1,load_ws):
         ws.append(row)
+def readCell(addList,load_ws):
+    all_values=[]
+    for row in range(addList[1],addList[3]+1): #row 단위로 읽기
+        row_value=[] #row 마다 셀의 값을 저장
+        for cell in range(addList[0],addList[2]+1): #row 마다 cell 단위로 데이터 읽기
+            row_value.append(load_ws.cell(row,cell).value) #row_value에 cell의 내용을 넣어주어 row를 완성
+        all_values.append(row_value) #row를 전체 리스트에 추가    
+    return all_values
